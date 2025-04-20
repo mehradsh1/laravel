c033e6d7a2229bf30a7a266a389d91f056ae37f3
Mehrad <a.mehrno47@gmail.com>
2025-04-20 14:53:22 +0330

    add'tamrin view '

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,15 @@
+<h1>salam bar pj gole golab</h1>
+
+<script>
+    const hobbies = {{\Illuminate\Support\Js::from($hobbies)}}
+</script>
+
+@verbatim
+<div>
+    Name: {{name}}
+    Age: {{age}}
+    Job: {{job}}
+    Hobbies: {{hobbies}}
+    @if
+</div>
+@endverbatim
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,9 @@
 <?php
 
-
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ProductControler;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
-    return view('welcome');
-});
+Route::get('/',[HomeController::class , 'index'])->name('home');
 Route::view('/about','about')->name('about')->name('about');
 
-//Route::controller(CarController::class)->group(function (){
- //   Route::get('/car',  'index');
-   // Route::get('/my-car',  'mycars');
-//});
 
-Route::apiResources([
-    'car' => CarController::class,
-    'product' => ProductControler::class,
-
-]);
